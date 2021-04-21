8a4bd76c5c271af12c590e76d89853fb6a38f001
Damian Soo <damian.soo24@gmail.com>
2021-04-21 15:18:43 +1200

    A program to find all unique tags

---
+++ tagfinder.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+
+    FILE *input = fopen("testwsj.xml", "r");
+    char tags[20][20];
+    char character;
+    while ((character = fgetc(input)) != EOF) {
+
+    }
+}
